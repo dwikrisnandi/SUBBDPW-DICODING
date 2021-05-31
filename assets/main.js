f9aaf5036730b1a3bcda1930d60d5ec4473d062a
Dwi Krisnandi <dwikrisnandi8@gmail.com>
2021-05-31 19:34:59 +0700

    SUB BDPW

---
+++ assets/main.js
@@ -0,0 +1,13 @@
+function myFunction() {
+    document.getElementsByClassName("topnav")[0].classList.toggle
+("responsive");
+}
+
+console.log("JavaScript berhasil digunakan");
+
+//edited profile
+document.getElementById('nama').innerHTML = 'Dwi Krisnandi';
+document.getElementById('kota').innerHTML = 'Kab. Karawang';
+document.getElementById('agama').innerHTML = 'Islam';
+document.getElementById('instansi').innerHTML = 'SMK Negeri 1 Tirtajaya Kab. Karawang';
+
